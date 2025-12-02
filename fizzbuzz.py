aa1e63f9f8d9c38e8b5c80aeb996eeabf42dbc46
wsj2000m <wsj2000m@gmail.com>
2025-12-02 10:18:40 +0900

    ok feat: Print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,8 @@
-for i in range(16,30+1):
-    if i%3 == 0:
+for i in range(12,30+1):
+    if i%15 == 0:
+        print('fizzbuzz')
+    elif i%3==0:
         print('fizz')
-
     elif i%5 == 0:
         print('buzz')
     else:
